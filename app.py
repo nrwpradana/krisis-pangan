1daa557a870b4e8d174fa9dad36036636e01a5f1
nrwpradana <nadhiar11@gmail.com>
2022-10-13 10:23:33 +0700

    chart size

--- app.py
+++ app.py
@@ -42,7 +42,11 @@ st.markdown('<p style="font-family:Arial; color:Black; font-size: 16px;">\
             unsafe_allow_html=True)
 
 html_string = '''
-<iframe title="Commodity price changes in 2022" aria-label="Column Chart" id="datawrapper-chart-BI2gD" src="https://datawrapper.dwcdn.net/BI2gD/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="400"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
+<iframe title="Commodity price changes in 2022" aria-label="Column Chart" id="datawrapper-chart-BI2gD" 
+src="https://datawrapper.dwcdn.net/BI2gD/2/" scrolling="no" frameborder="0" style="width: 100; min-width: 100% !important; 
+border: none;" height="400"></iframe><script type="text/javascript">!function(){"use strict";
+window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");
+for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
 '''
 
 components.html(html_string)  # JavaScript works
