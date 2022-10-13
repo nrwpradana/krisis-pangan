62a41499d9aec97a54899e98c7865e1b7c77facc
nrwpradana <nadhiar11@gmail.com>
2022-10-13 10:25:20 +0700

    height

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ st.markdown('<p style="font-family:Arial; color:Black; font-size: 16px;">\
 
 html_string = '''
 <iframe title="Commodity price changes in 2022" aria-label="Column Chart" id="datawrapper-chart-BI2gD" 
-src="https://datawrapper.dwcdn.net/BI2gD/2/" scrolling="no" frameborder="0" style="width: 100; min-width: 100% !important; 
-border: none;" height="400"></iframe><script type="text/javascript">!function(){"use strict";
+src="https://datawrapper.dwcdn.net/BI2gD/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; 
+border: none;" height="800"></iframe><script type="text/javascript">!function(){"use strict";
 window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");
 for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
 
