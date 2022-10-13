3a9759e316134facd899d863bfdccb6b23c8d7e4
nrwpradana <nadhiar11@gmail.com>
2022-10-13 10:24:33 +0700

    tes

--- app.py
+++ app.py
@@ -47,6 +47,11 @@ src="https://datawrapper.dwcdn.net/BI2gD/2/" scrolling="no" frameborder="0" styl
 border: none;" height="400"></iframe><script type="text/javascript">!function(){"use strict";
 window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");
 for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
+
+
+
+
+
 '''
 
 components.html(html_string)  # JavaScript works
