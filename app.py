3d1653ff6b8e782b80ca587b2bce26cf1daf1697
nrwpradana <nadhiar11@gmail.com>
2022-10-13 10:33:48 +0700

    atur chart

--- app.py
+++ app.py
@@ -46,14 +46,13 @@ st.markdown("""<hr style="height:4px;border:none;color:#333;background-color:#33
 st.header("A. Perubahan Harga Komoditas Tahun 2022")
 
 html_string = '''
-<iframe aria-label="Column Chart" id="datawrapper-chart-BI2gD" 
+<iframe title="Commodity price changes in 2022" aria-label="Column Chart" id="datawrapper-chart-BI2gD" 
 src="https://datawrapper.dwcdn.net/BI2gD/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; 
 border: none;" height="400"></iframe><script type="text/javascript">!function(){"use strict";
 window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");
 for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();</script>
 '''
-
-components.html(html_string,height=600)
+components.html(html_string)
 
 st.header("B. Ukraina & Rusia merupakan Negara Penting dalam Pasar Agrifood Dunia")
 
