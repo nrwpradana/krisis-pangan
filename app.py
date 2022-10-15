c85a19ac2fc473022083ede01ba22bc91002b5b6
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-15 19:27:28 +0700

    h2

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ st.markdown("---")
 
 
 #st.header("B. Ukraina & Rusia merupakan Negara Penting dalam Pasar Agrifood Dunia")
-st.markdown('<h1 style="font-family:Arial; font-size:34; background-color: skyblue">'+
-            'B. Ukraina & Rusia merupakan Negara Penting dalam Pasar Agrifood Dunia</h1>', unsafe_allow_html=True)
+st.markdown('<h2 style="font-family:Arial;background-color: skyblue">'+
+            'B. Ukraina & Rusia merupakan Negara Penting dalam Pasar Agrifood Dunia</h2>', unsafe_allow_html=True)
 
 
 # Global Export Fraction 
