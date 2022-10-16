81c1e866f02e21cdd7d42498753d1f1c5cdaff2c
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-16 19:25:12 +0700

    Center

--- app.py
+++ app.py
@@ -109,9 +109,9 @@ with export_4:
             </h5>', unsafe_allow_html=True)
 st.markdown("---") 
 
-st.markdown('<h5 style="font-family:Arial; background-color: snow">'+
+st.markdown('<h5 style="font-family:Papyrus; background-color: seashell"><center>'+
             'Banyak negara berkembang sangat bergantung pada impor pangan pertanian dari Ukraina dan Rusia*\
-            </h5>', unsafe_allow_html=True)
+            </center></h5>', unsafe_allow_html=True)
 
 #Summary
 #st.markdown('#### Observasi 75 negara non-Eropa yang dianggap negara berkembang dan kurang berkembang*:')
