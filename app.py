30f6916b2a70e567174009d2d80d91d3b129990e
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-17 17:36:24 +0700

    Edit Section D

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ st.markdown('<h2 style="font-family:Arial;background-color: skyblue">'+
 st.markdown('<h5 style="font-family:Papyrus; background-color: white">'+
             'Konflik Ukraina-Rusia memberikan dampak yang besar bagi pasokan bahan pangan dan pertanian global, termasuk dengan Indonesia yang memiliki hubungan perdagangan dengan Ukraina dan Rusia. \
             </h5>', unsafe_allow_html=True)
-st.markdown(""">*<h5 style="font-family:Papyrus; background-color: white">'+
+st.markdown(""">*<h5 style="font-family:Papyrus; background-color: white">
             'Konflik Ukraina-Rusia memberikan dampak yang besar bagi pasokan bahan pangan dan pertanian global, termasuk dengan Indonesia yang memiliki hubungan perdagangan dengan Ukraina dan Rusia. \
             </h5>*""", unsafe_allow_html=True)
 ua_ina, ru_ina = st.tabs(["Ukraina -> Indonesia", "Rusia -> Indonesia"])
