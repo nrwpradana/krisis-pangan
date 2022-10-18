57a0f8e4bfd07b9ed0eeb81ebef17ed8ef48e015
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-18 08:09:09 +0700

    Resize

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ with ua_ina:
             ' Section Under Construction \
             </strike>⚙️</h5>', unsafe_allow_html=True)
     ukraina_string = """
-    <div style='min-width: 40 cm; min-height: 30 cm ;'><iframe class='embed-responsive-item' src='https://comtrade.tradingeconomics.com/comtrade/share?r=ukr&c=0000&v=treemapmarkets&t=2&title=' style='border:none; scrolling='no''></iframe></div>
+    <iframe class='embed-responsive-item' src='https://comtrade.tradingeconomics.com/comtrade/share?r=ukr&c=0000&v=treemapmarkets&t=2&title=Ekspor Ukraina' style='border:none; scrolling='no';min-width: 100%;height="400"'></iframe></div>
     """
     components.html(ukraina_string)
 
