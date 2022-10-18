049a2010bc07261045117a1a78a92c9ec601af86
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-18 13:21:03 +0700

    Edit Title

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ with ua_ina:
 
 with ru_ina:
     rusia_string="""
-    <iframe class='embed-responsive-item' width='100%' height='400px' src='https://comtrade.tradingeconomics.com/comtrade/share?r=rus&c=0000&v=treemapmarkets&t=2&title=' style='border:none; scrolling='no''></iframe>
+    <iframe class='embed-responsive-item' width='100%' height='400px' src='https://comtrade.tradingeconomics.com/comtrade/share?r=rus&c=0000&v=treemapmarkets&t=2&title=Ekspor Global Rusia' style='border:none; scrolling='no''></iframe>
     """
     components.html(rusia_string,height=405)
     st.caption('Sumber : Russia Exports By Country, https://tradingeconomics.com/russia/exports-by-country.')
